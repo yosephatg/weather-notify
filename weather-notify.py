e93fd16d44a7c619fef4c2b57e685ba83a031d03
yoseph tesfagaber <joseph.ahferom@gmail.com>
2017-02-15 21:04:41 -0500

    setting up env

---
+++ weather-notify.py
@@ -0,0 +1,17 @@
+# Yoseph Tesfagaber
+# Pythong 2.7.12
+# weather-notify.js
+
+import requests
+import json
+
+# yoseph to-dos
+# define url
+
+# call api, save as json
+
+# parse json, format only info i want 
+# log as debug mode
+
+# read twilio creds from another file
+# send sms via twilio
